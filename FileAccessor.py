11a14ad5589c6c2571c1b3b65b62ecba57c7e734
Ryan <friess@umich.edu>
2024-11-10 15:07:15 -0500

    updated FileAccessor.py

--- FileAccessor.py
+++ FileAccessor.py
@@ -1,16 +1,43 @@
 #first file in repo
 #open .nc file 
 import netCDF4 as nc
-fn = '/Users/samba/Documents/WeatherData/file1.nc'
+fn = '/Users/ryanfriess/Desktop/solarcar/cloud-cover/nc-files/file1.nc'
 ds = nc.Dataset(fn)
 
+def process_array(mask_array):
+    new_array = [[0 for _ in range(len(mask_array[0]))] for _ in range(len(mask_array))]
+    for r in range(len(mask_array)):
+        for c in range(len(mask_array[0])):
+            new_array[r][c] = mask_array[r][c]
 
+    return new_array
+
+def write_to_file(fn, img):
+    height, width = str(len(img)), str(len(img[0]))
+    fwith open("/output-files/"+fn, 'w') as f:
+        f.write("P3\n")
+        f.write(width + " " + height +"\n")
+        f.write("255\n")  # Maximum color value
+
+        # Write pixel data
+        for r in range(len(img)):x
+            f.write(f"{r} {g} {b} ")
+        f.write("\n")
 
 # Access the Cloud_Mask variable
 cloud_mask = ds.groups['geophysical_data'].variables['Cloud_Mask'][:]
 
 # Print out the cloud mask data
-print(cloud_mask[0])
+print("Number of images (first dimension of cloud_mask):" , len(cloud_mask))
+for i in range(len(cloud_mask)):
+    print("Dimension of image " + str(i) + ":", len(cloud_mask[i]), len(cloud_mask[i][i]))
+    mask_array = cloud_mask[i]
+    # print(mask_array[0][0])
+    processed = process_array(mask_array)
+
+
+
+
 
 
 '''
