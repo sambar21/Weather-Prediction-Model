6a9dc1a850864b1d093a0573debebfb06612f01c
sambar21 <sambatur@umich.edu>
2024-11-11 16:12:47 -0500

    Visualization Model

--- VisualizationModel.py
+++ VisualizationModel.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 
-# Open the NetCDF file
+
 fn = '/Users/samba/Documents/WeatherData/file1.nc'
 ds = nc.Dataset(fn)
 
@@ -13,11 +13,11 @@ print(f"Cloud Mask Shape: {cloud_mask.shape}")
 # Loop through each layer of the cloud mask data
 for i in range(cloud_mask.shape[0]):
    plt.figure(figsize=(10, 8)) 
-   plt.imshow(cloud_mask[i], cmap='gray', interpolation='nearest')  # Use 'gray' colormap for grayscale
+   plt.imshow(cloud_mask[i], cmap='gray', interpolation='nearest')  
    plt.colorbar(label='Cloud Mask Value')  
    plt.title(f'Cloud Mask Visualization - Layer {i + 1}')  
    plt.axis('off')  
    plt.show()  
 
-# Close the dataset after use
+
 ds.close()
